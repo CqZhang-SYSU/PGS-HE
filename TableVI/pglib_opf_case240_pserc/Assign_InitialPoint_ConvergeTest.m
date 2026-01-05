7024596b2bde11cbb6a340d65cb531b91f5e9b9c
CuiqingZhang <zhangcq6@mail2.sysu.edu.cn>
2026-01-05 15:29:05 -0500

    Refactor and clean up the convergence test script

--- TableVI/pglib_opf_case240_pserc/Assign_InitialPoint_ConvergeTest.m
+++ TableVI/pglib_opf_case240_pserc/Assign_InitialPoint_ConvergeTest.m
@@ -1,9 +1,9 @@
 clear; clc;
 
 % This script implements the pglib_opf_case240_pserc system 
-% using MIPS, IPOPT with the MUMPS linear solver, IPOPT with the map7 linear solver,
+% using MIPS, IPOPT with the MUMPS linear solver, IPOPT with the ma97 linear solver,
 % KNITRO using Interior-Point/Barrier Direct algorithm,
-% and KNITRO using  Quadratic Programming (SQP) algorithm.
+% and KNITRO using  Quadratic Programming (SQP) algorithm
 % under the same feasible point.
 
 
@@ -131,4 +131,5 @@ om = opf_setup(mpc, mpopt);
 tic;
 [results, success, raw] = nlpopf_solver_For_arbitrary_initpoint(x0, om, mpopt); 
 total_computation_time_KNITRO_IP = total_computation_time_KNITRO_IP + toc;
+
 fprintf('total computation time of KNITRO-SQP: %d\n',total_computation_time_KNITRO_IP);
